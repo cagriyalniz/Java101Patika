20a09a9718791b66d8161aaab1c01cd538144324
cagriyalniz <cagriyalniz@gmail.com>
2022-03-10 16:48:18 +0300

    input islemleri eklendi

--- src/metotlar_siniflar_diziler_OOP/SalaryCalc/Main.java
+++ src/metotlar_siniflar_diziler_OOP/SalaryCalc/Main.java
@@ -1,10 +1,21 @@
 package metotlar_siniflar_diziler_OOP.SalaryCalc;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Employee emp = new Employee("ali", 2000, 45, 1985);
+        Scanner inp = new Scanner(System.in);
+        System.out.println("çalışanın adını giriniz");
+        String name = inp.nextLine();
+        System.out.println("çalışanın maaşını giriniz");
+        int salary = inp.nextInt();
+        System.out.println("çalışanın haftalik calisma saatini giriniz");
+        int workHours = inp.nextInt();
+        System.out.println("çalışanın ise baslayis yilini giriniz");
+        int hireYear = inp.nextInt();
+        Employee emp = new Employee(name, salary, workHours, hireYear);
 
+        System.out.println("çalışanın yeni maaşı hesaplaniyor ...:.:.:.:.:.:.:.:.::.:.:.:.:.:.:.:.:...");
         emp.toString(emp);
 
     }
